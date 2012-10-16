649e8c771136d1e9013e1e1895e1f1c1565df1b7
Tyler Garlick <tjgarlick@gmail.com>
2012-10-16 11:01:59 -0600

    Fixed default route

--- LocationSolution.Mvc.Web/App_Start/RouteConfig.cs
+++ LocationSolution.Mvc.Web/App_Start/RouteConfig.cs
@@ -14,15 +14,9 @@ namespace LocationSolution.Mvc.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "StatesRoutes",
-                url: "Countries/{countryCode}/States/{action}/{id}",
-                defaults: new { controller = "States", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Countries", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
